45a1eab28e622a27754cc4ebc11887cbf2f1f267
aumpwn0804 <99827692+aumpwn0804@users.noreply.github.com>
2024-06-22 16:41:21 +0700

    Synchronizing job reddit-glue-job from AWS Glue at 2024/06/22 09:41:21

--- aws-glue-job/.py
+++ aws-glue-job/.py
@@ -4,7 +4,7 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-from pyspark.sql.function import concat_ws
+from pyspark.sql.functions import concat_ws
 from awsglue import Dynamicframe
 
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
