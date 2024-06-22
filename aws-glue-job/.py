3ccc0f05543f15c156f5ebc96295436d13337eae
aumpwn0804 <99827692+aumpwn0804@users.noreply.github.com>
2024-06-22 16:38:48 +0700

    Synchronizing job reddit-glue-job from AWS Glue at 2024/06/22 09:38:48

---
+++ aws-glue-job/.py
@@ -0,0 +1,34 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from pyspark.sql.function import concat_ws
+from awsglue import Dynamicframe
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Script generated for node Amazon S3
+AmazonS3_node1718995592881 = glueContext.create_dynamic_frame.from_options(format_options={"quoteChar": "\"", "withHeader": True, "separator": ",", "optimizePerformance": False}, connection_type="s3", format="csv", connection_options={"paths": ["s3://reddit-engineer/raw/"], "recurse": True}, transformation_ctx="AmazonS3_node1718995592881")
+
+# convert Dynamicframe to Dataframe
+df = AmazonS3_node1718995592881.toDF()
+
+# concatenate the three columns into a single columns
+df_combined = df.withColumn('ESS_updated', concat_ws(df['edited'],df['spoiler'],df['stickied']))
+df_combined = df_combined.drop('edited', 'spoiler', 'stickied')
+
+# convert back to Dynamicframe
+S3bucket_node_combined = Dynamicframe.formDF(df_combined, glueContext, 'S3bucket_node_combined')
+
+# Script generated for node Amazon S3
+AmazonS3_node1718995595602 = glueContext.write_dynamic_frame.from_options(frame=S3bucket_node_combined, connection_type="s3", format="csv",
+    connection_options={"path": "s3://reddit-engineer/tranformed/", "partitionKeys": []}, transformation_ctx="AmazonS3_node1718995595602")
+
+job.commit()
